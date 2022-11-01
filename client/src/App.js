4f34faf2a542c0d88fbe83e8c5ad28ea20019005
sophia pung <sophiagracepung@gmail.com>
2022-10-31 20:05:51 -0700

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import GraphvisPage from './components/graphVis'
+
+function App() {
+  return (
+    <div className="App">
+      <GraphvisPage/>
+    </div>
+  );
+}
+
+export default App;
